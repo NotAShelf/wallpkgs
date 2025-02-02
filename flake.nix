bd56d6c493bc7338000842663b07362085fc1462
Artur Manuel <balkenix@outlook.com>
2025-02-02 13:30:59 +0000

    refactor(treewide): cleanup; remove dupes

--- flake.nix
+++ flake.nix
@@ -21,36 +21,39 @@
     wallpapers = let
       fileExts = ["png" "jpg" "jpeg" "gif"];
     in
-      builtins.map (n: let
-        findFile = builtins.filter builtins.pathExists (map (ext: ./wallpapers/${n}.${ext}) fileExts);
-        file =
-          if findFile == []
-          then builtins.throw "Either ${n} isn't a file or it doesn't have the ${builtins.concatStringsSep ", " (lib.lists.init fileExts)} or ${lib.lists.last fileExts} extensions."
-          else builtins.head findFile;
-      in {
-        path = file;
-        tags = lib.splitString "-" n;
-        hash = builtins.hashFile "sha256" file;
-      })
-      (map (n: builtins.head (lib.splitString "." n)) (
-        builtins.attrNames (
-          lib.filterAttrs (n: _: n != "README.md") (builtins.readDir ./wallpapers)
-        )
-      ));
+      builtins.listToAttrs (map (n: let
+          findFile = builtins.filter builtins.pathExists (map (ext: ./wallpapers/${n}.${ext}) fileExts);
+          file =
+            if findFile == []
+            then builtins.throw "Either ${n} isn't a file or it doesn't have the ${builtins.concatStringsSep ", " (lib.init fileExts)} or ${lib.last fileExts} extensions."
+            else builtins.head findFile;
+        in {
+          name = n;
+          value = {
+            path = file;
+            tags = lib.splitString "-" n;
+            hash = builtins.hashFile "sha256" file;
+          };
+        })
+        (map (n: builtins.head (lib.splitString "." n)) (
+          builtins.attrNames (
+            lib.filterAttrs (n: _: n != "README.md") (builtins.readDir ./wallpapers)
+          )
+        )));
 
     packages = genSystems (_:
       builtins.listToAttrs (map (n: {
           name = n;
           value = throw ''
             As of #15, wallpkgs no longer uses packages.$${system} for wallpapers, as wallpapers
-            should never have been system-dependant. All wallpapers have been moved to a top-level
+            should never have been system-dependent. All wallpapers have been moved to a top-level
             'wallpapers' attribute under the repository.
 
             To use your own wallpapers, you must conform to the new filename format.
 
-            For example, 'inputs.wallpkgs.packages.wallpapers.catppuccin-01' now refers to what used
-            to reside in /share/wallpapers/catppuccin/01.png. To suppress this error, get wallpapers
-            from the packages output.
+            For example, 'inputs.wallpkgs.wallpapers.catppuccin-01' now refers to what used to
+            reside in /share/wallpapers/catppuccin/01.png. To suppress this error, get wallpapers
+            from the wallpapers output.
           '';
         }) [
           "catppuccin"
